8938ea8e2dd717ba7bc6bcfa70a840b38cc449dd
67372a <117533205+67372a@users.noreply.github.com>
2025-01-01 18:16:45 -0600

    Add RAdamScheduleFree to optimizer list

--- ui_files/OptimizerUI.py
+++ ui_files/OptimizerUI.py
@@ -181,6 +181,7 @@ class Ui_optimizer_ui(object):
         self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"LPFAdamW", None))
         self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"Lion", None))
         self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"Prodigy", None))
+        self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"RAdamScheduleFree", None))
         self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"RMSProp", None))
         self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"RMSPropADOPT", None))
         self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"RMSPropADOPTMARS", None))
