d15020b0bc4b5534b349b52150153c8e7f994e78
machina <117533205+67372a@users.noreply.github.com>
2025-10-06 15:15:38 -0500

    Add SCGOpt

--- ui_files/OptimizerUI.py
+++ ui_files/OptimizerUI.py
@@ -204,6 +204,7 @@ class Ui_optimizer_ui(object):
         self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"SGDSaI", None))
         self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"ScalableShampoo", None))
         self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"ScheduleFreeWrapper", None))
+        self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"SCGOpt", None))
         self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"SCORN", None))
         self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"SCORNMachina", None))
         self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"SimplifiedAdEMAMix", None))
