e9779d230bbb65cbf75b3f32dd1389920023a804
67372a <117533205+67372a@users.noreply.github.com>
2026-05-26 14:51:44 -0500

    NorMuonScheduleFree init, fix sdxl inpaint mask bug

--- ui_files/OptimizerUI.py
+++ ui_files/OptimizerUI.py
@@ -198,6 +198,7 @@ class Ui_optimizer_ui(object):
         self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"Lion_adv", None))
         self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"MomentusCaution", None))
         self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"Mythical", None))
+        self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"NorMuonScheduleFree", None))
         self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"OAGOpt", None))
         self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"Prodigy", None))
         self.optimizer_type_selector.addItem(QCoreApplication.translate("optimizer_ui", u"ProdigyPlusScheduleFree", None))
